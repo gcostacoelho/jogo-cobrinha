5ec9f187cb1aea0f9c36c9500b4f6620c9aa9f8d
gcostacoelho <gcostacoelho2003@gmail.com>
2021-09-03 14:03:36 -0300

    third-commit

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ snake[0] = {
 }
 
 let direction = "right";
+let fruit = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 
 function createBG(){
@@ -22,8 +26,28 @@ function createSnake(){
     }
 }
 
+function createFruit(){
+    context.fillStyle = "red";
+    context.fillRect(fruit.x,fruit.y, box, box);
+}
+
+document.addEventListener("keydown", update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != 'right') direction='left';
+    if(event.keyCode == 38 && direction != 'down') direction='up';
+    if(event.keyCode == 39 && direction != 'left') direction='right';
+    if(event.keyCode == 40 && direction != 'up') direction='down';
+}
+
 function startGame(){
+    if(snake[0].x > 15 * box && direction =='right') snake[0].x=0;
+    if(snake[0].x < 0 * box && direction =='left') snake[0].x=16*box;
+    if(snake[0].y > 15 * box && direction =='down') snake[0].y=0;
+    if(snake[0].y < 0 * box && direction =='up') snake[0].y=16*box;
+
     createBG();
+    createFruit()
     createSnake();
 
     let snakeX = snake[0].x;
@@ -31,8 +55,8 @@ function startGame(){
 
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY += box;
-    if(direction == "down") snakeY -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
 
     snake.pop();
 
